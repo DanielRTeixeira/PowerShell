f85e6c24ee3bf4bc07309da81e2fd00b079e77ed
DanielRTeixeira <DanielRTeixeira@users.noreply.github.com>
2015-03-17 16:29:31 +0000

    Create SID.ps1

---
+++ SID/SID.ps1
@@ -0,0 +1,3 @@
+$objUser = New-Object System.Security.Principal.NTAccount($env:UserDomain, $env:UserName)
+$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+$strSID.Value
