ca04f575989accf3c3b72beab9ed105795c1780c
DanielRTeixeira <DanielRTeixeira@users.noreply.github.com>
2015-03-18 14:11:50 +0000

    Delete FSMOs.ps1

--- FSMOs/FSMOs.ps1
+++
@@ -1,4 +0,0 @@
-function Get-FSMOs {
-  Get-ADForest $env:UserDomain | Format-Table SchemaMaster,DomainNamingMaster
-  Get-ADDomain $env:UserDomain | Format-Table PDCEmulator,RIDMaster,InfrastructureMaster
-}
