57615a22063e6f67fa25fb07e9754b039388e5a4
DanielRTeixeira <DanielRTeixeira@users.noreply.github.com>
2015-03-18 14:12:17 +0000

    Delete SID.ps1

--- SID/SID.ps1
+++
@@ -1,5 +0,0 @@
-function Get-SID {
-  $objUser = New-Object System.Security.Principal.NTAccount($env:UserDomain, $env:UserName)
-  $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
-  $strSID.Value
-}
